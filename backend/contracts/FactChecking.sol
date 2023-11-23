9758cba848886ea8bad1d3ab6a62762eb8b81453
root <root@DESKTOP-IA0TRAI>
2023-11-23 16:19:55 +0100

    first commit

---
+++ backend/contracts/FactChecking.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.22;
+
+contract FactChecking {
+
+    // Variables 
+
+    struct writer {
+        address,
+ 
+    }
+
+    struct document {
+        address,
+
+    }
+
+    struct checker {
+        address,
+
+    }
+
+
+
+
+
+}
